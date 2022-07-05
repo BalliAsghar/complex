cd2ab017518e97428bdddda94982487e8f195d4c
Balli Asghar <ballihacker@outlook.com>
2022-07-05 16:11:38 +0100

    added keys

---
+++ server/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+};
