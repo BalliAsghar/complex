3cfc89b568481b8169d35eb97ea1b27318c18591
Balli Asghar <ballihacker@outlook.com>
2022-07-08 20:31:00 +0100

    .

---
+++ client1/main.js
@@ -0,0 +1,9 @@
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.store("app", {
+  name: "Asghar",
+});
+
+Alpine.start();
--- worker/index.js
+++ worker/index.js
@@ -2,10 +2,14 @@ const keys = require("./keys");
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
-  retry_strategy: () => 1000,
+  url: `redis://${keys.redisHost}:6379`,
 });
+
+redisClient
+  .connect()
+  .then(() => console.log("Connected to Redis At Worker"))
+  .catch((err) => console.log(err));
+
 const sub = redisClient.duplicate();
 
 function fib(index) {
@@ -14,6 +18,7 @@ function fib(index) {
 }
 
 sub.on("message", (channel, message) => {
-  redisClient.hset("values", message, fib(parseInt(message)));
+  redisClient.hSet("values", message, fib(parseInt(message)));
 });
+
 sub.subscribe("insert");
