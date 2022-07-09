bc031e3ecc451c28103920d5bda3523bc082dc77
Balli Asghar <ballihacker@outlook.com>
2022-07-09 21:06:13 +0100

    basic server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import Express from "express";
+
+const app = Express();
+app.use(Express.json());
+
+app.post("/message", async (req: Express.Request, res: Express.Response) => {
+  const { message } = req.body;
+  console.log(message);
+  res.send("OK");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
