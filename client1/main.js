732a8a8aaa53d3c50741a362d07c73439f54da2d
Balli Asghar <ballihacker@outlook.com>
2022-07-09 20:46:50 +0100

    lets that time again

--- client1/main.js
+++
@@ -1,9 +0,0 @@
-import Alpine from "alpinejs";
-
-window.Alpine = Alpine;
-
-Alpine.store("app", {
-  name: "Asghar",
-});
-
-Alpine.start();
--- worker/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  redisHost: process.env.REDIS_HOST,
-};
