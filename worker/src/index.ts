86a8aeb8f1db8f5843633526c54da6a6e3caed0e
Balli Asghar <ballihacker@outlook.com>
2022-07-09 22:07:19 +0100

    added consumer

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+import { Kafka } from "kafkajs";
+
+async function main() {
+  const kafka = new Kafka({
+    clientId: "worker",
+    brokers: ["balliasgharsair:9092"],
+  });
+
+  const consumer = kafka.consumer({
+    groupId: "worker-group",
+  });
+
+  await consumer.connect();
+  await consumer.subscribe({
+    topic: "message",
+  });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log(
+        `Received message ${message.toString()}, from Topic ${topic}`
+      );
+    },
+  });
+}
+
+main();
