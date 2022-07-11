6a667a7201c0db45d2c04eab5d15ac1ce9ba9ccd
Balli Asghar <ballihacker@outlook.com>
2022-07-12 00:04:51 +0100

    Had to wait for kafka to load

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,9 +1,17 @@
 import { Kafka } from "kafkajs";
 import { SendMessage } from "./lib/slack";
+import { config } from "dotenv";
+
+config();
 async function main() {
   const kafka = new Kafka({
     clientId: process.env.KAFKA_CLIENT_ID,
     brokers: [`${process.env.KAFKA_HOST}:${process.env.KAFKA_PORT}`],
+    connectionTimeout: 10000,
+    retry: {
+      initialRetryTime: 100,
+      retries: 10,
+    },
   });
 
   const consumer = kafka.consumer({
@@ -20,7 +28,7 @@ async function main() {
       console.log(
         `Received message: ${message?.value?.toString()}, from Topic ${topic}`
       );
-      // await SendMessage(message?.value?.toString()!);
+      await SendMessage(message?.value?.toString()!);
     },
   });
 }
