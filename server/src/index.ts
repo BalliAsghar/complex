9d4e19da4da579dd02da21fcb8b20eddc1dbfe99
Balli Asghar <ballihacker@outlook.com>
2022-07-13 00:37:14 +0100

    added route to get all messages from db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import Express from "express";
 import { relayMessage } from "./lib/kafka";
+import { getMessages } from "./prisma/db";
 const app = Express();
 app.use(Express.json());
 
@@ -7,6 +8,12 @@ app.post("/message", async (req: Express.Request, res: Express.Response) => {
   const { message } = req.body;
   await relayMessage(process.env.KAFKA_TOPIC!, message);
   res.send("Message sent");
+});
+
+app.get("/messages", async (req: Express.Request, res: Express.Response) => {
+  // const { topic } = req.body;
+  const messages = await getMessages("message");
+  res.json(messages);
 });
 
 const PORT = process.env.PORT || 3000;
