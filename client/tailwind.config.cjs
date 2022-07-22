9a7636e51fb293e512840373ff24f84dd87e90d3
Balli Asghar <ballihacker@outlook.com>
2022-07-23 00:11:50 +0100

    safe point

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["**/*.html", "**/*.md", "**/*.yml", "**/*.json"],
+  darkMode: "class",
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      cor: ["Cormorant SC", "serif"],
+    },
   },
   plugins: [],
 };
