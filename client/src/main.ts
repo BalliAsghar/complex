f8776875e0f1745b7ca20b5e370c814525dfb3ba
Balli Asghar <ballihacker@outlook.com>
2022-07-22 01:53:02 +0100

    added client

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,33 @@
 import Alpine from "alpinejs";
+import "./index.css";
 
 window.Alpine = Alpine;
 
+Alpine.store("hero", "Send Message Via Kafka");
+
 Alpine.store("app", {
-  title: "Hello World",
+  init() {
+    fetch("http://localhost:3000/messages")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.messages = data;
+      });
+  },
+  message: "",
+  messages: [],
+  sendMessage() {
+    fetch("http://localhost:3000/message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: this.message,
+      }),
+    });
+    this.message = "";
+  },
 });
+
 Alpine.start();
--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,6 @@
+import { Alpine as AlpineType } from "alpinejs";
 /// <reference types="vite/client" />
+
+declare global {
+  var Alpine: AlpineType;
+}
