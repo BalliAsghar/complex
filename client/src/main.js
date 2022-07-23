d296f18ca386713a7c6ba20791f44bfddfe50df0
Balli Asghar <ballihacker@outlook.com>
2022-07-23 01:28:48 +0100

    sending message to kafka

--- client/src/main.js
+++ client/src/main.js
@@ -20,6 +20,22 @@ Alpine.store("app", {
   formatDate(date) {
     return dayjs(date).fromNow();
   },
+  sendMessage() {
+    async function postMessage(message) {
+      const body = JSON.stringify({ message });
+      const response = await fetch("http://localhost:3000/message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body,
+      });
+      const data = await response.json();
+      console.log(data);
+    }
+    postMessage(this.message);
+    this.message = "";
+  },
 });
 
 Alpine.start();
