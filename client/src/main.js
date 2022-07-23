25cb51a429dd94af9e6b2fb03c5c5f7c8dd6c709
Balli Asghar <ballihacker@outlook.com>
2022-07-23 01:14:30 +0100

    fetching messages

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,25 @@
 import Alpine from "alpinejs";
-
+import * as dayjs from "dayjs";
+import * as relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 window.Alpine = Alpine;
 
 Alpine.store("title", "Send Message Via Kafka");
+
+Alpine.store("app", {
+  init() {
+    async function getMessages(messages) {
+      const response = await fetch("http://localhost:3000/messages");
+      const data = await response.json();
+      messages.push(...data);
+    }
+    getMessages(this.messages);
+  },
+  message: "",
+  messages: [],
+  formatDate(date) {
+    return dayjs(date).fromNow();
+  },
+});
 
 Alpine.start();
